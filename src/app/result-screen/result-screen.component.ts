2043cadc235f85b8fba332467839be8413b622c1
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-02-23 16:43:58 -0600

    save function
    code navigation to clear history
    
    I want ability to use assignments in home and result screens and save both to home screen list

--- src/app/result-screen/result-screen.component.ts
+++ src/app/result-screen/result-screen.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-result-screen',
   templateUrl: './result-screen.component.html',
   styleUrls: ['./result-screen.component.css']
 })
+
 export class ResultScreenComponent {
-  // constructor(
-  //   private location: Location
-  // ) {};
+  constructor(
+    private location: Location
+  ) {};
 
-  // endRoll = () => {
-  //   this.location.historyGo(-2);
-  // };
-
+    saveTreasure = () => {
+      this.location.historyGo(-2);
+    };
 }
