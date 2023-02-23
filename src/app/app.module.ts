40f1d009d7e03f49167240a91f4ed7111cbba0b1
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-02-23 12:41:17 -0600

    components generated
    added components for home, parameter, and results screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { ResultScreenComponent } from './result-screen/result-screen.component';
+import { ParametersScreenComponent } from './parameters-screen/parameters-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    ResultScreenComponent,
+    ParametersScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/parameters-screen/parameters-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parameters-screen',
+  templateUrl: './parameters-screen.component.html',
+  styleUrls: ['./parameters-screen.component.css']
+})
+export class ParametersScreenComponent {
+
+}
