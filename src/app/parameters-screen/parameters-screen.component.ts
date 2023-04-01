11cf9016be7156b65c6568c8ecb52c381eba8a82
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-31 21:18:41 -0500

    week 8 tutorial
    following the model

--- src/app/parameters-screen/parameters-screen.component.ts
+++ src/app/parameters-screen/parameters-screen.component.ts
@@ -1,5 +1,5 @@
 import { TreasureSvcService } from './../treasure-svc.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,26 @@ import { Router } from '@angular/router';
   templateUrl: './parameters-screen.component.html',
   styleUrls: ['./parameters-screen.component.css']
 })
-export class ParametersScreenComponent {
+export class ParametersScreenComponent implements OnInit {
 
   constructor(
     private router: Router,
     private svc: TreasureSvcService
   ) {};
+
+  extraPlayers: {
+    name: string;
+    checked: boolean;
+  }[] = [];
+
+  ngOnInit(): void {
+    this.extraPlayers = this.svc.getPlayerNames().map(x => ({
+      name: x,
+      checked: false
+    }));
+
+    console.log(this.extraPlayers);
+  };
 
   rollTreasure = () => {
     this.router.navigateByUrl("result-screen");
