07a854a75017b7ac683704ce75707e523366144c
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-10 20:49:39 -0500

    removed component
    this isn't needed

--- src/app/hoard-screen/hoard-screen.component.ts
+++ src/app/hoard-screen/hoard-screen.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { TreasureSvcService } from '../treasure-svc.service';
 import { Router } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-hoard-screen',
   templateUrl: './hoard-screen.component.html',
-  styleUrls: ['./hoard-screen.component.css']
+  styleUrls: ['./hoard-screen.component.css'],  
+  standalone: true,
+  imports: [NgbCollapseModule],
 })
 export class HoardScreenComponent implements OnInit {
+  visible = [false];
   constructor (
     private loc: Location,
     private svc: TreasureSvcService,
@@ -16,6 +20,12 @@ export class HoardScreenComponent implements OnInit {
   ) {}
 
   showMe = this.svc.itsTwo;
+
+  
+
+  toggleCollapse = (i: number) => {
+    this.visible[i] = !this.visible[i];
+  }
   
   updateHoard = () => {
     this.loc.historyGo(-1);
