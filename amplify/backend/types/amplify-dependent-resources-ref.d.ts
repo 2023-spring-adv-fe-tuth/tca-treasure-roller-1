ac87941a52a866923b939d9ec1fdd18deb1881d2
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-15 20:03:09 -0500

    kill amplify
    probably will not work but it makes me feel better...

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "tcatreasureroller1": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
